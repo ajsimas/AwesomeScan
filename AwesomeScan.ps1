af8fcd0ccac27d06a9c70191d97ab72cc14cefd4
ajsimas <me@ajsimas.com>
2017-12-10 16:47:31 -0700

    Add files via upload

---
+++ AwesomeScan.ps1
@@ -0,0 +1,27 @@
+﻿$CCleanerURL = "http://download.piriform.com/ccsetup537.exe"
+$CCleanerPath = "C:\Windows\Temp\ccsetup537.exe"
+$CCleanerIniURL = "https://goo.gl/hnqhnP"
+$CCleanerIniPath = "C:\Program Files\CCleaner\ccleaner.ini"
+
+If(!(Test-Path 'C:\Program Files\CCleaner')){
+
+    $CCleaner = New-Object System.Net.WebClient
+    $CCleaner.DownloadFile($CCleanerURL, $CCleanerPath)
+
+    Write-Output "Installing CCleaner"
+
+    Start-Process -FilePath "C:\Windows\Temp\ccsetup537.exe" -ArgumentList /S
+
+    Start-Sleep 30
+
+    $CCleanerIni = New-Object System.Net.WebClient
+    $CCleanerIni.DownloadFile($CCleanerIniURL, $CCleanerIniPath)
+}
+
+else{
+    Write-Output "Already Installed"
+}
+
+#Update CCleaner
+
+Start-Process -FilePath "C:\Program Files\CCleaner\CCleaner64.exe" -ArgumentList /Auto
